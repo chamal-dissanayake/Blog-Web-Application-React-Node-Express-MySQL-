fc224a3a1c50452b168938501ce0678ddb740b2c
Chamal Dissanayake <65418556+chamal-dissanayake@users.noreply.github.com>
2023-05-19 00:51:12 +0530

    Initial commit

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "../img/logo.png";
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src={Logo} alt="" />
+      <span>
+        Try to Build Blog App Using <b>React.js</b>.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
